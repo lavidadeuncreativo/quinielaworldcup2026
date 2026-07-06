c5c58734c319bc452cc82803301a57bb275df742
lavidadeuncreativo <lavidadeuncreativo@gmail.com>
2026-07-06 13:08:56 -0600

    Map participants to uploaded profile photos

---
+++ lib/participantPhotos.ts
@@ -0,0 +1,11 @@
+export const participantPhotos: Record<string,string> = {
+  israel: "/participantes/israel-cabrera.svg",
+  isra: "/participantes/isra-chico.svg",
+  alfre: "/participantes/tio-alfre.svg",
+  liz: "/participantes/liz-flores.svg",
+  rebeca: "/participantes/rebeca-granados.svg",
+  alfredito: "/participantes/alfredito.svg",
+  nuria: "/participantes/nuria.svg",
+  pedro: "/participantes/pedro.svg",
+  rebe: "/participantes/rebe-mama.svg",
+};
